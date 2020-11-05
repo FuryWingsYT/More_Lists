0d5b79c980be326f6dc2af2d26f23580ee436b3a
FuryWingsYT <32018052+FuryWingsYT@users.noreply.github.com>
2020-11-05 15:02:55 -0500

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,8 +1,21 @@
 def sum_of_odd_nums(n):
-    pass
+    odds=[]
+    for i in range(1, n+1):
+        odds.append(i*2+1)
+    b=0
+    for j in odds:
+        b+=j
+    return b
 
 def caesar_cipher(message, key):
-    pass
+    msg = []
+    for c in message:
+        msg.append(ord(c)+key)
+    msg2 = []
+    for b in msg:
+        msg2.append(chr(b))
+        
+    return msg2
 
 def fizzbuzz(n):
     pass 
@@ -16,7 +29,7 @@ def main():
 
     print()
 
-    ciphertext = "Frpsxwhu#Vflhqfh#lv#qr#pruh#derxw#frpsxwhuv#wkdq#dvwurqrp|#lv#derxw#whohvfrshv1"
+    ciphertext = "bazinga"
     print(caesar_cipher(ciphertext, -3))
 
     print()
